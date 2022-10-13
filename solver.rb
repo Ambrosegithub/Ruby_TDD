5bb9230264b87594a0fd32225974efd8625cef86
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-10-13 10:07:53 +0200

    Add solver class for tdd

--- solver.rb
+++ solver.rb
@@ -1,26 +1,24 @@
+# frozen_string_literal: true
 
-Isigba Ambrose
-1:50 AM
 class Solver
   def factorial(number)
-      if number < 0
-          return "number must be greater than or equal to 0"
-      end
-      if number == 0
-          return 1
-      end
-      return number * factorial(number-1)
+    return 'number must be greater than or equal to 0' if number.negative?
+    return 1 if number.zero?
+
+    number * factorial(number - 1)
   end
+
   def reverse(string)
-      if string == ""
-          return ""
-      end
-      return string.reverse
+    return '' if string == ''
+
+    string.reverse
   end
+
   def fizzbuzz(number)
-      return 'fizzbuzz' if (number %3 == 0 && number %5 == 0)
-      return 'fizz' if (number %3 == 0)
-      return 'buzz' if (number %5 == 0)
-      return number
-    end
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number
+  end
 end
