d04e9800e3e1ae6d0040e82590689218963fab5a
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-10-13 07:06:28 +0200

    Add factorial methos to solver

--- solver.rb
+++ solver.rb
@@ -17,6 +17,7 @@ class Solver
       end
       return string.reverse
   end
+  
   def fizzbuzz(number)
       return 'fizzbuzz' if (number %3 == 0 && number %5 == 0)
       return 'fizz' if (number %3 == 0)
