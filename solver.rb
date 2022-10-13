47a30100583b574c02cad72addea24daedb0898a
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-10-13 07:08:05 +0200

    Create a method called reverse that takes one argument

--- solver.rb
+++ solver.rb
@@ -17,7 +17,6 @@ class Solver
       end
       return string.reverse
   end
-  
   def fizzbuzz(number)
       return 'fizzbuzz' if (number %3 == 0 && number %5 == 0)
       return 'fizz' if (number %3 == 0)
